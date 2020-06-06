9f2fcb06b3e004fb0a7585dee7eb39d71964389c
MonseCruz <rmonserrat@gmail.com>
2020-06-06 16:48:38 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-import novios from './img/novios1.svg'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-import PrimaryButton from './components/PrimaryButton'
-import SecondaryButton from './components/SecondaryButton'
-import ImageContainer from './components/ImgContainer'
-import Footer from './components/Footer'
+import './App.css'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-      <PrimaryButton name={"INICIAR SESIÓN"} />
-      <SecondaryButton name={"CERRAR SESIÓN"} />
-      <ImageContainer imageUrl={novios} />
-      <Footer />
-      
-      
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+   
+  }
+
+  render () {
+    
+    return (
+      <Router>
+        <div className="App">
+          <Navbar/>
+         {/*  <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/Notes">
+              <Note isUserLogedIn={isUserLogedIn}/>
+            </Route>
+            <Route exact path="/Converter">
+              <Converter />
+            </Route>
+            <Route exact path="/CreditCard">
+              <CreditCard />
+            </Route>
+            <Route exact path="/Hooks">
+              <Hooks />
+            </Route>
+          </Switch> */}
+        </div>
+      </Router>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// CSS
+import "./Navbar.css";
+import logo from '../../img/logo-color.svg'
+import hamburger from '../../img/hamburger-menu.png'
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuActive: false,
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.logOut = this.logOut.bind(this)
+  }
+  toggleMenu() {
+    const { isMenuActive } = this.state;
+    this.setState({
+      isMenuActive: !isMenuActive,
+    });
+  }
+  logOut() {
+    this.props.logOut()
+    this.toggleMenu()
+  }
+  render() {
+    const { isUserLogedIn } = this.props
+    const { isMenuActive } = this.state;
+    const menuClass = isMenuActive ? "Menu-active" : "";
+    return (
+    <div>
+      <div className="desktop">
+        <div className="nav">
+          <img src={logo} className="logo"/> 
+          <ul>
+              <li>
+                <Link to="/Perfil">Perfil</Link>
+                </li>
+                <li>
+                <Link to="/Evento">Evento</Link>
+              </li>
+              <li>
+                <Link to="/logout">Salir</Link>
+              </li>
+            {/* <li>
+              {
+                isUserLogedIn ? (
+                  <li>
+                    <Link to="/Perfil" onClick={this.toggleMenu}>Perfil</Link>
+                  </li>
+                ) : null
+              }
+              </li>
+              <li>
+              {
+                isUserLogedIn ? (
+                  <li>
+                    <Link to="/Evento" onClick={this.toggleMenu}>Evento</Link>
+                  </li>
+                ) : null
+              }
+            </li> */}
+          </ul>
+        </div>
+      </div>
+      <div className="movil">
+        <div className="nav">
+          <img src={logo} className="logo"/> <button onClick={this.toggleMenu} className="Menu-button"><img src={hamburger}/></button>
+        </div>
+        <div className={`Menu ${menuClass}`}>
+          <h2>Hola user</h2>
+          <nav>
+            <div>
+              <ul>
+                <li>
+                <Link to="/Perfil">Perfil</Link>
+                </li>
+                <li>
+                <Link to="/Evento">Evento</Link>
+                </li>
+                <li>
+                <Link to="/logout">Salir</Link>
+              </li>
+              {/* <li>
+                {
+                  isUserLogedIn ? (
+                    <li>
+                      <Link to="/Perfil" onClick={this.toggleMenu}>Perfil</Link>
+                    </li>
+                  ) : null
+                }
+                </li>
+                <li>
+                {
+                  isUserLogedIn ? (
+                    <li>
+                      <Link to="/Evento" onClick={this.toggleMenu}>Evento</Link>
+                    </li>
+                  ) : null
+                }
+                </li> */}
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
